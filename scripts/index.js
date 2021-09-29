3631548659e635077c8aa465c8489d0482c82e13
Prottay Ray <prottayray@gmail.com>
2021-09-29 19:31:28 +0530

    query in localstorage and redirection

--- scripts/index.js
+++ scripts/index.js
@@ -11,7 +11,7 @@ let travellerCount = document.getElementById('travellerCount');
 let add = [document.getElementById('adult_add'), document.getElementById('children_add'), document.getElementById('infant_add')];
 let del = [document.getElementById('adult_del'), document.getElementById('children_del'), document.getElementById('infant_del')];
 let special = document.getElementsByName('travl');
-let travelClass = document.getElementsByName('classes');
+let travelClass = document.getElementsByName('flightClass');
 let swap = document.getElementById('swap');
 let classOption = document.getElementById('classOption');
 let travellerOption = document.getElementById('travellerOption');
@@ -20,8 +20,38 @@ let class_click = document.getElementById('travellerClass');
 let one = document.getElementById('one');
 let round = document.getElementById('round');
 let multi = document.getElementById('multi');
+let finalSearch = document.getElementById('finalSearch');
 
-let travellers = 1, trip = 1, specialCitizen = 0, flightClass = 1, totalCount = 1;
+let trip = "one", specialCitizen = 0, flightClass = 1, totalCount = 1;
+
+function query(from, to, depart, arrive, travellers, adults, children, infants, flightClass, special, trip) {
+    this.from = from;
+    this.to = to;
+    this.depart = depart;
+    this.arrive = arrive;
+    this.travellerCount = travellers;
+    this.adultCount = adults;
+    this.childrenCount = children;
+    this.infantCount = infants;
+    this.flightClass = flightClass;
+    this.specialCitizen = special;
+    this.trip = trip;
+}
+
+finalSearch.addEventListener('click', () => {
+
+    if (from.value === "") {
+        alert('Please enter valid origin city');
+        return;
+    }
+    if (to.value === "") {
+        alert('Please enter valid destination city');
+        return;
+    }
+    localStorage.setItem('searchQuery', JSON.stringify(new query(from.value, to.value, depart.value, arrive.value, totalCount, Number(adultCount.innerText), Number(childrenCount.innerText), Number(infantCount.innerText), travelClass.innerText, specialCitizen, trip)));
+    window.location.href = "flightSearch.htm";
+
+});
 
 travellerCountDone.addEventListener('click', () => {
     travellerOption.style.display = 'none';
@@ -72,7 +102,7 @@ del[2].addEventListener('click', () => {
 one.addEventListener('click', () => {
     one.style.backgroundColor = "white"
     one.style.color = "#1853A2";
-    trip = 1;
+    trip = "one";
     arrive.style.color = "gray";
     arrive.setAttribute('disabled', 'true');
     round.style.backgroundColor = "transparent";
@@ -84,7 +114,7 @@ round.addEventListener('click', roundTrip);
 multi.addEventListener('click', () => {
     multi.style.backgroundColor = "white"
     multi.style.color = "#1853A2";
-    trip = 3; 
+    trip = "multi"; 
     round.style.backgroundColor = "transparent";
     round.style.color = "#CAD5F5";
     one.style.backgroundColor = "transparent";
@@ -94,7 +124,7 @@ multi.addEventListener('click', () => {
 function roundTrip() {
     round.style.backgroundColor = "white"
     round.style.color = "#1853A2";
-    trip = 2; 
+    trip = "round"; 
     arrive.style.color = "black";
     arrive.disabled = false;
     one.style.backgroundColor = "transparent";
