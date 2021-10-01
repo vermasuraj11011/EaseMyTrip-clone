0ce95dc96adba491b46a0b2a27785c68d5ccba47
Suraj Verma <vermasuraj0008@gmail.com>
2021-10-01 22:21:28 +0530

    adding users schema to the server.js

--- server.js
+++ server.js
@@ -29,6 +29,19 @@ const flightSchema = new mongoose.Schema(
 
 const Flight = new mongoose.model('flights', flightSchema);
 
+const UserSchema = new mongoose.Schema(
+	{
+		email_id: { type: String, required: true },
+		password: { type: String, required: true }
+	},
+	{
+		versionKey: false, // will not show _v
+		timestamps: true // createdAt, updatedAt
+	}
+);
+
+const User = new mongoose.model('user', UserSchema);
+
 app.get('/flights', async (req, res) => {
 	const flights = await Flight.find().lean().exec(); //db.users.find() lean() is used  to make user json object and exec is used to return proper promise
 
@@ -40,6 +53,11 @@ app.get('/flights/:id', async (req, res) => {
 
 	return res.status(200).send({ flight });
 });
+// app.get('/flights/:date', async (req, res) => {
+// 	const flight = await Flight.find({ journey_date: req.params.date }).lean().exec();
+
+// 	return res.status(200).send({ flight });
+// });
 
 app.get('/flights/:source/:destination', async (req, res) => {
 	const flight = await Flight.find({ source: req.params.source, destination: req.params.destination }).lean().exec();
@@ -52,7 +70,7 @@ app.get('/flights/date/:source/:destination/:date', async (req, res) => {
 		destination: req.params.destination,
 		journey_date: req.params.date
 	})
-		.sort({ takeoff_time: -1 })
+		.sort({ takeoff_time: 1 })
 		.lean()
 		.exec();
 
@@ -70,6 +88,13 @@ app.get('/flights/price/:source/:destination/:date', async (req, res) => {
 		.exec();
 
 	return res.status(200).send({ flight });
+});
+
+// adding user data in the data base
+app.post('/users', async (req, res) => {
+	const user = await User.create(req.body); // db.users.insert()
+
+	return res.status(201).send({ user });
 });
 
 app.listen(5005, async () => {
